a14dc1de54d1c36f661ce37af08e93071a29bfe3
Liam Roberts <liamroberts77@googlemail.com>
2020-10-21 15:26:37 +0100

    added menu toggle hamburger to x on click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,9 @@ import React, { useState} from 'react'
 import { Link } from "react-router-dom";
 
 function Navbar() {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
     return (
         <>
             <nav className="navbar">
@@ -9,6 +12,9 @@ function Navbar() {
                     <Link to="/" className="navbar-logo">
                         PERFECT <i class="fas fa-meteor"></i>
                     </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                    </div>
                 </div>
             </nav>
         </>

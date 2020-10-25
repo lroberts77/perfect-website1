34ef54255f8b686b85dc452ce5ff1bc9b4b88dec
Liam Roberts <liamroberts77@googlemail.com>
2020-10-25 23:51:55 +0000

    added responsive sizing for button in navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState} from 'react'
 import { Link } from "react-router-dom";
+import { Button } from './button';
 
 function Navbar() {
     const [click, setClick] = useState(false)

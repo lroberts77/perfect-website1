ebc51c1a93e9caa3a33832afce62ccd32c795120
Liam Roberts <liamroberts77@googlemail.com>
2020-10-25 23:37:37 +0000

    added styling for buttons in button.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ function Navbar() {
                     <ul className={click ? "nav-menu active" : "nav-menu"}>
                         <li className="nav-item">
                             <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                                Home
+                                HOME
                             </Link>
                         </li>
                         <li className="nav-item">

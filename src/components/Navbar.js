1cd2c305ad5cabb46de285adae931e5a89f441b5
Liam Roberts <liamroberts77@googlemail.com>
2020-10-26 23:09:19 +0000

    added styling for every element in app.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState} from 'react'
 import { Link } from "react-router-dom";
 import { Button } from './button';
+import "./Navbar.css";
 
 function Navbar() {
     const [click, setClick] = useState(false)
@@ -20,7 +21,7 @@ function Navbar() {
     return (
         <>
             <nav className="navbar">
-                <div className="navbar-contents">
+                <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
                         PERFECT <i class="fas fa-meteor"></i>
                     </Link>

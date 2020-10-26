270c9ddec83846680170c897323dee2fe4a900f6
Liam Roberts <liamroberts77@googlemail.com>
2020-10-26 23:23:54 +0000

    added react function boiler plate in herosection.js

---
+++ src/components/HeroSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+    return (
+        <div className="hero-container">
+            <video />
+        </div>
+    )
+}
+
+export default HeroSection

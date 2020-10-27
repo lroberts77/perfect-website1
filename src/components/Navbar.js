cb5c9e4989c27124f93b3394d46f063ee013e735
Liam Roberts <liamroberts77@googlemail.com>
2020-10-27 17:08:57 +0000

    added home file in pages in components folder which returns <Herosection />

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
-import { Button } from './button';
+import { Button } from './Button';
 import "./Navbar.css";
 
 function Navbar() {

ef385e1077b11cb73befb9b6ef7750c4be0faa96
Liam Roberts <liamroberts77@googlemail.com>
2020-10-27 18:57:55 +0000

    added styling for hero-container h1 and p in herosection.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
-import Home from "./components/pages/Home"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './components/pages/Home';
 
 function App() {
   return (
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import "../App.css"
-import { Button } from './Button'
-import "./HeroSection.css"
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
 
 function HeroSection() {
     return (
-        <div className="hero-container">
-            <video src=".videos/video1.mp4" autoPlay loop muted />
-            <h1>HELLO</h1>
-            <p>HI</p>
-            <div className="hero-btns">
-                <Button
-                className="btns"
-                buttonStyle="btn--outline"
-                buttonSize="btn--large">
-                    GET STARTED
-                </Button>
-                <Button
-                className="btns"
-                buttonStyle="btn--primary"
-                buttonSize="btn--large">
-                    WATCH TRAILER <i className="far fa-play-circle" />
-                    </Button>
-            </div>
+      <div className='hero-container'>
+        <video src="public/videos/video-2.mp4" autoPlay loop muted />
+        <h1>HELLO</h1>
+        <p>HI</p>
+        <div className='hero-btns'>
+            <Button
+            className='btns'
+            buttonStyle='btn--outline'
+            buttonSize='btn--large'>
+            GET STARTED
+            </Button>
+            <Button
+            className='btns'
+            buttonStyle='btn--primary'
+            buttonSize='btn--large'>
+            WATCH TRAILER <i className='far fa-play-circle' />
+            </Button>
         </div>
-
-    )
+      </div>
+    );
 }
 
-export default HeroSection
+export default HeroSection;

a236aa1ea64bec380c224dd30b6663cc89ce07fa
Liam Roberts <liamroberts77@googlemail.com>
2020-11-03 17:23:50 +0000

    added second card with image and text

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,8 +2,7 @@ import React from 'react'
 import CardItem from './CardItem'
 import  "./Cards.css";
 import see from '../assets/see.jpg'
-
-
+import eclipse from '../assets/eclipse.jpg'
 
 export default function Cards() {
     return (
@@ -15,6 +14,11 @@ export default function Cards() {
                         <CardItem src={see} 
                         text="summary or explanation for card 1" 
                         label="CARD 1" 
+                        path="/services"
+                        />
+                        <CardItem src={eclipse} 
+                        text="summary or explanation for card 2" 
+                        label="CARD 2" 
                         path="/services"
                         />
                     </ul>

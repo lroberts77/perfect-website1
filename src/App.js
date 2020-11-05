9b639ac442e3623dd025cbe96b1290f92d240da9
Liam Roberts <liamroberts77@googlemail.com>
2020-11-05 15:49:52 +0000

    added route path for products

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
       </Switch>
     </Router>
     </>

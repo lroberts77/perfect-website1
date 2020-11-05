24e56e97ea30cf760b02844e4719a66687393721
Liam Roberts <liamroberts77@googlemail.com>
2020-11-05 15:53:49 +0000

    added route path for signup

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/services" component={Services} />
         <Route path="/products" component={Products} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
     </Router>
     </>

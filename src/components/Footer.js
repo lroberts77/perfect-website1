d8f88ea48b39fca2db2cb3d7d65849382dc69a18
Liam Roberts <liamroberts77@googlemail.com>
2020-11-05 16:39:40 +0000

    added skeleton footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Button } from './Button'
+
+export default function Footer() {
+    return (
+        <div className="footer-container">
+            <section className="footer-subscription">
+                <p className="footer-subscription-heading">
+                    JOIN
+                </p>
+                <p className="footer-subsciption-text">
+                    CONTACT
+                </p>
+                <form>
+                    <input type="email" name="email" placeholder="Your Eamil" className="footer-input" />
+                    <Button buttonStyle="btn--outline">SEND</Button>
+                </form>
+            </section>
+        </div>
+    )
+}

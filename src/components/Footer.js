e88f4c00c88b47a60529f7beb663ac361d24f370
Liam Roberts <liamroberts77@googlemail.com>
2020-11-07 22:05:02 +0000

    fixed a typo in footer.css and added display flex to footer-link-wrapper

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,22 +13,48 @@ export default function Footer() {
                 <p className="footer-subsciption-text">
                     CONTACT
                 </p>
+                <div className="input-areas">
                 <form>
                     <input type="email" name="email" placeholder="Your Eamil" className="footer-input" />
                     <Button buttonStyle="btn--outline">SEND</Button>
                 </form>
+                </div>
             </section>
             <div className="footer-links">
-                <div className="footer-link-wrapper">
-                    <div className="footer-links-items">
-                        <h2>About us</h2>
-                        <Link to="/">test1</Link>
-                        <Link to="/">test2</Link>
-                        <Link to="/">test3</Link>
-                        <Link to="/">test4</Link>
-                    </div>
-                </div>
-            </div>
+            <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+      </div>
             <section className="social-media">
                 <div className="social-media-wrap">
                     <div className="footer-logo">

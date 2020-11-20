25dc5dbdf091a119993cc8fbdfb445f302d2f6fd
Liam Roberts <liamroberts77@googlemail.com>
2020-11-20 21:32:30 +0000

    changed mobile size sign up button to contact

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -54,7 +54,7 @@ export default function Navbar() {
                             </Link>
                         </li>
                     </ul>
-                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                    {button && <Button buttonStyle='btn--outline'>CONTACT</Button>}
                 </div>
             </nav>
         </>
